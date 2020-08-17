948659ffac584e024e1bad8d82c7d46da9502776
sylvain <sylvain.labarre@live.fr>
2020-08-17 15:59:09 +0200

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
-<script>
-import NavLink from './components/NavLink.vue'
-
-export default {
-  name: 'App',
-  components: {
-    NavLink
-  }
-}
-</script>
-
 <template>
-  <div id="app"> 
-    <nav>
-      <NavLink url="/" text="Accueil" />
-      <NavLink url="/about" text="A propos" />
-      <NavLink url="/contact" text="Contact" />
-    </nav>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
@@ -26,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
