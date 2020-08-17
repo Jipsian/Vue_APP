f8dc84c0ad38b970f8b4927088b58657eda71c58
sylvain <sylvain.labarre@live.fr>
2020-08-17 15:39:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import NavLink from './components/NavLink.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavLink
+  }
+}
+</script>
+
+<template>
+  <div id="app"> 
+    <nav>
+      <NavLink url="/" text="Accueil" />
+      <NavLink url="/about" text="A propos" />
+      <NavLink url="/contact" text="Contact" />
+    </nav>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
